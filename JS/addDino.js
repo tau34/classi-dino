e30290195a7dcefa3e2bbcf10e1d5d59723edb1d
tau34 <109936561+tau34@users.noreply.github.com>
2023-04-25 08:03:19 +0900

    Add files via upload

--- JS/addDino.js
+++ JS/addDino.js
@@ -1,9 +1,9 @@
 var isDisplaying = false;
 var isExist = false;
+var isLocking = true;
 
 window.addEventListener("keydown", function (e) {
-    if (e.code == "KeyD") {
-        console.log(isExist)
+    if (e.code == "KeyD" && !(isLocking)) {
         if (!isExist) {
             $("body").append("<iframe src=\"https://tsd-chrome-dino.vercel.app/\" width=\"800\" height=\"250\" class=\"dino\" style=\"position: fixed; bottom: 10px; right: 10px;\"></iframe>");
             isExist = true;
@@ -15,6 +15,10 @@ window.addEventListener("keydown", function (e) {
             $(".dino").css("display", "initial");
             isDisplaying = true;
         }
+    } else if (e.code == "F2") {
+        isLocking = !isLocking;
+        $(".dino").css("display", "none");
+        isDisplaying = false;
     } else {
         return false;
     }
